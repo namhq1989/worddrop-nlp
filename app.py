db68de3b0c103c313293317c279e3e538ab974ea
Nam HQ <namhq.1989@gmail.com>
2025-03-20 07:35:18 +0700

    chore(server): add code to run server

--- app.py
+++ app.py
@@ -286,3 +286,7 @@ def analyze_word():
     
     print("[LOG] Response ready")
     return jsonify(result)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=port)
